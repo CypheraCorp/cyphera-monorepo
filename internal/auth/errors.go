31ba60d8966c92f7fbd55d080717027ababceb6b
natefikru <nate.fikru@gmail.com>
2025-01-26 13:05:05 -0500

    updated text

--- internal/auth/errors.go
+++ internal/auth/errors.go
@@ -3,6 +3,6 @@ package auth
 import "errors"
 
 var (
-	ErrNoValidatedClaims = errors.New("no validated claims found in request context")
+	ErrNoValidatedClaims = errors.New("no validated claims found in request ctx")
 	ErrInvalidSubject    = errors.New("invalid subject claim")
 )
