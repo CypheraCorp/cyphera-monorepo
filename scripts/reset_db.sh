bcf3e20543fa10e134248344daf9548c9a1c3a84
natefikru <natefikru@gmail.com>
2025-02-09 20:52:47 -0500

    update logging for prod

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Database connection parameters
-DB_HOST="localhost"
-# DB_HOST="cyphera-db.cjwqc0yo6vzw.us-east-1.rds.amazonaws.com"
+#DB_HOST="localhost"
+DB_HOST="cyphera-db.cjwqc0yo6vzw.us-east-1.rds.amazonaws.com"
 DB_PORT="5432"
 DB_USER="postgres"
 DB_NAME="cyphera"
